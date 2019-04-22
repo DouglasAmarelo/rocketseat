1c6afd914b221908dab0612ab504727fcce4152a
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-04-22 14:43:28 -0300

    Utilizando o express - Parametros

--- modulo1/index.js
+++ modulo1/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	return res.send('Hello World');
+	return res.send(`Bem-vindo, ${req.query.name}`);
 });
 
-app.get('/login', (req, res) => {
-	return res.send('Login');
+app.get('/nome/:name/', (req, res) => {
+	return res.json({
+		message: `Bem-vindo ${req.params.name}`
+	});
 });
 
 app.listen(3000);
