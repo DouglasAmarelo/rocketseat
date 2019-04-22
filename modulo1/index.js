d82841acd855d4cd2f5824f2d5651932014f66a8
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-04-22 19:20:19 -0300

    nunjucks

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,30 +1,28 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-
-const logMiddleware = (req, res, next) => {
-	console.log(`
-		HOST: ${req.headers.host} |
-		URL: ${req.url} |
-		METHOD: ${req.method}
-	`);
-
-	req.appName = `GoNode`;
-
-	return next();
-};
-
-app.use(logMiddleware);
-
-app.get('/', (req, res) => {
-	return res.send(`Bem-vindo, ${req.appName} ${req.query.name}`);
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app,
+	watch: true
 });
 
-app.get('/nome/:name/', (req, res) => {
-	return res.json({
-		message: `Welcome ${req.params.name}`
-	});
+app.set('view engine', 'njk');
+
+const users = [
+	'Bruno Annunciato',
+	'Douglas Lopes',
+	'Guilherme Paiva'
+];
+
+app.get('/', (req, res) => {
+	return res.render('list', { users });
+});
+
+app.get('/new', (req, res) => {
+	return res.render('new')
 });
 
 
