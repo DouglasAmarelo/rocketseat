700fadf24b3f9aa378428ea5eb7480ef26f98131
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-04-22 18:26:38 -0300

    Middleware

--- modulo1/index.js
+++ modulo1/index.js
@@ -2,14 +2,30 @@ const express = require('express');
 
 const app = express();
 
+
+const logMiddleware = (req, res, next) => {
+	console.log(`
+		HOST: ${req.headers.host} |
+		URL: ${req.url} |
+		METHOD: ${req.method}
+	`);
+
+	req.appName = `GoNode`;
+
+	return next();
+};
+
+app.use(logMiddleware);
+
 app.get('/', (req, res) => {
-	return res.send(`Bem-vindo, ${req.query.name}`);
+	return res.send(`Bem-vindo, ${req.appName} ${req.query.name}`);
 });
 
 app.get('/nome/:name/', (req, res) => {
 	return res.json({
-		message: `Bem-vindo ${req.params.name}`
+		message: `Welcome ${req.params.name}`
 	});
 });
+
 
 app.listen(3000);
