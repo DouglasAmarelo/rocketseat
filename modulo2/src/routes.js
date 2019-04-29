b4461fae619578aca86eb823d496e18d70f0a6c7
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-04-29 11:36:11 -0300

    Modulo 2

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const UserController = require('./app/controllers/UserController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-	return res.send('Hello World!');
-});
+routes.get('/signup', UserController.create);
+routes.post('/signup', UserController.store);
 
 module.exports = routes;
