19b96732a799f4c070fb37fbcff36dcd94982569
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-05-04 11:51:48 -0300

    Login e autenticação Verificação de login middlewares de tratamento de sessão

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const multerConfig = require('./config/multer');
 const upload = require('multer')(multerConfig);
+const routes = express.Router();
 
+// Controllers
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 
-const routes = express.Router();
+// Middlewares
+const authMiddleware = require('./app/middlewares/auth');
+const guestMiddleware = require('./app/middlewares/guest');
 
-// SIGNIN
-routes.get('/', SessionController.create);
+// SIGNIN - Login
+routes.get('/', guestMiddleware, SessionController.create);
 routes.post('/signin', SessionController.store);
 
-// SIGNUP
-routes.get('/signup', UserController.create);
+// SIGNUP - Cadastro
+routes.get('/signup', guestMiddleware, UserController.create);
 routes.post('/signup', upload.single('avatar'), UserController.store);
+
+// DASHBOARD
+routes.use('/app', authMiddleware);
+routes.get('/app/dashboard', (req, res) => {
+	console.log('REQ USER', req.session.user);
+	return res.render('dashboard');
+});
 
 module.exports = routes;
