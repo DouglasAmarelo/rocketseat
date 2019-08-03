f0d7ac965805ace922de13bf42fa61658904f0da
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-08-03 08:09:15 -0300

    Modulo 3 v2

---
+++ goreact/modulo3-v2/src/pages/main/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FavoriteActions from '../../store/actions/favorites';
+
+class Main extends Component {
+	static propTypes = {
+		addFavorites: PropTypes.func.isRequired,
+		favorite: PropTypes.arrayOf(PropTypes.shape({
+			id: PropTypes.number,
+			name: PropTypes.string,
+			description: PropTypes.string,
+			url: PropTypes.string,
+		})).isRequired
+	};
+
+	state = {
+		repositoryInput: ''
+	};
+
+	handleRepository = (e) => {
+		e.preventDefault();
+
+		this.props.addFavorite();
+	};
+
+	render() {
+		return (
+			<>
+				<form onSubmit={this.handleRepository}>
+					<input
+						placeholder="usuário/repositório"
+						type="text"
+						value={this.state.repositoryInput}
+						onChange={e => this.setState({ repositoryInput: e.target.value })}
+					/>
+					<button type="submit">Adicionar</button>
+				</form>
+
+				<ul>
+					{this.props.favorites.map(favorite => (
+						<li key={favorite.id}>
+							<p>
+								<strong>{favorite.name}</strong> ({favorite.description})
+							</p>
+							<a href={favorite.url}>Acessar</a>
+						</li>
+					))}
+				</ul>
+			</>
+		);
+	};
+};
+
+const mapStateToProps = state => ({
+	favorites: state.favorites
+});
+
+const mapDispatchToProps = dispatch => {
+	return bindActionCreators(FavoriteActions, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
---
+++ goreact/modulo3-v2/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import reduces from './reducers';
+
+const composer = process.env.NODE_ENV === 'development'
+	? compose(
+		applyMiddleware(...[]),
+		console.tron.createEnhancer()
+	)
+	:
+		applyMiddleware(...[])
+	;
+
+const store = createStore(reduces, composer);
+
+export default store;
